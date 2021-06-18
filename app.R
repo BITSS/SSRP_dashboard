97b0c6ebc769c93aaabda26d5de784f7a53b48fe
Fernando Hoces de la Guardia <fhoces@users.noreply.github.com>
2021-06-18 08:47:22 -0700

    reshaping the data

--- app.R
+++ app.R
@@ -33,6 +33,8 @@ ui <- fluidPage(
 )
 
 
+claims_max <- df1 %>% select(contains("S2_4Q4_2P")) %>% names() %>% length()
+
 #loading the data
 df1 <- read_csv("all_submitted_may_25_2021.csv") %>% 
     mutate(clean_doi = str_extract(
@@ -40,21 +42,75 @@ df1 <- read_csv("all_submitted_may_25_2021.csv") %>%
         regex("10\\.\\d{4,9}/[-._;()/:a-z0-9]+$",   #Explain regex
               ignore_case = T)
     )) %>%
+    mutate(similar_title = phonetic(S0_1Q1P2)) %>% 
     group_by(email, clean_doi) %>%
     mutate(repro_id = cur_group_id()) %>%          #unique identifier for each repro
     ungroup() %>%
-    select(repro_id, contains("S2_4Q4_2P")) %>%     #keep levels only
+    select(
+        repro_id,
+        clean_doi,
+        similar_title,
+        S0_1Q1P2,
+        S1_3Q1P1,
+        S2_4iP2,
+        contains("S2_4Q4_2P"),
+        contains("S1_3Q5P")
+    ) %>% 
+    select(contains("r1c")) %>%      #keep levels only
     pivot_longer(#reshape to long
-        cols = !repro_id,
-        names_to = "claims",
-        values_to = "rep_level") %>%
+        cols = !c(repro_id,clean_doi, similar_title, S0_1Q1P2, S1_3Q1P1),   # do not modify: repro id, title id, title, claim summ
+        names_to = c("claims"),
+        values_to = "rep_level" ) %>%
     filter(!is.na(rep_level)) %>%
     group_by(repro_id) %>%
     mutate(
         mean_rl = mean(rep_level),
         max_rl = max(rep_level),
         min_rl = min(rep_level)
-    )
+    ) %>% 
+    arrange(similar_title, repro_id) %>% 
+    select(similar_title, S1_3Q1P1) 
+
+
+# Renaming display item names and s
+temp1 <- df1 %>% 
+    rename_at(.vars = paste0("S2_4Q4_2P", 1:claims_max),
+              .funs = ~ paste0("claim_score_", 1:claims_max)) %>% 
+    rename_at(.vars = paste0("S1_3Q5P", 1:claims_max, "_r1c1"),
+              .funs = ~ paste0("claim_di_main_", 1:claims_max)) %>% 
+    rename_at(.vars = paste0("S1_3Q5P", 1:claims_max, "_r1c2"),
+              .funs = ~ paste0("claim_di_alt_spec1_", 1:claims_max)) %>% 
+    rename_at(.vars = paste0("S1_3Q5P", 1:claims_max, "_r1c3"),
+              .funs = ~ paste0("claim_di_alt_spec2_", 1:claims_max)) %>% 
+    rename_at(.vars = paste0("S1_3Q5P", 1:claims_max, "_r1c4"),
+              .funs = ~ paste0("claim_di_alt_spec3_", 1:claims_max)) %>% 
+    rename_at(.vars = paste0("S1_3Q5P", 1:claims_max, "_r1c5"),
+              .funs = ~ paste0("claim_di_alt_spec4_", 1:claims_max)) %>% 
+    rename_at(.vars = paste0("S1_3Q5P", 1:claims_max, "_r1c6"),
+              .funs = ~ paste0("claim_di_alt_spec5_", 1:claims_max)) %>% 
+    select(contains("claim"), contains("S2_4iP1")) %>% select(contains("_1"), contains("S2_4iP1")) %>% View()
+    
+temp1 <- df1 
+
+
+df1 <- read_csv("all_submitted_may_25_2021.csv") %>% 
+    rename_at(.vars = paste0("S2_4Q4_2P", 1:claims_max),
+              .funs = ~ paste0("claim_score_", 1:claims_max)) %>% 
+    mutate(clean_doi = str_extract(
+        S0_1Q1P1,
+        regex("10\\.\\d{4,9}/[-._;()/:a-z0-9]+$",   #Explain regex
+              ignore_case = T)
+    )) %>%
+    mutate(similar_title = phonetic(S0_1Q1P2)) %>% 
+    group_by(email, clean_doi) %>%
+    mutate(repro_id = cur_group_id()) %>%
+    ungroup() %>% 
+    rename_at(.vars = paste0("S2_4iP", 1:claims_max, "_r1c6"),
+              .funs = ~ paste0("claim_di_alt_spec5_", 1:claims_max)) %>% 
+    select(repro_id, contains("claim_score"), contains("S2_4iP") ) %>% View()
+
+
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
